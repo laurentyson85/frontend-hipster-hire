2bcab641f7882bcdbed8bd8c75df534caaae7aed
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-22 16:25:20 -0600

    Add Switch and Route hooks to App

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,34 @@ import Nav from './Nav';
 import JobHome from './JobHome';
 import HipsterView from './HipsterView';
 import JobForm from './JobForm';
+import JobDetails from './JobDetails';
 
 
 
 
 function App() {
   return (    
-      
+      <div>
+        <Header />
+        <Nav />        
+        <Switch>        
+          <Route path="/hipsters">
+            <HipsterView/>
+          </Route>
+          <Route path="/:id">
+            <JobDetails />
+          </Route>
+          <Route path="/new_posting">
+            <JobForm/>
+          </Route>
+          <Route exact path="/">
+            <JobHome/>
+          </Route>
+          <Route path="*">
+            <h1>No Hipsters or Jobs Here</h1>
+          </Route>
+        </Switch>
+      </div>
     );
   }
 
