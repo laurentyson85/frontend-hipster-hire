55d0f105d6624cffc2282bca5d34f034909f8d37
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-22 16:18:19 -0600

    Add imports to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch} from 'react-router-dom';
+import Header from './Header';
+import Nav from './Nav';
+import JobHome from './JobHome';
+import HipsterView from './HipsterView';
+import JobForm from './JobForm';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return (    
+      
+    );
+  }
 
 export default App;
