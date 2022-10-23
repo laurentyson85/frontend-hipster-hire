ade3a550937858fd160f78bfb8918948944f1fee
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-22 20:13:46 -0600

    Add Header image and edit header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import headerImg from "../images/hipsterHeader.png"
 
 function Header() {
   return (
-    <div>hold</div>
-    
-  )
+    <div className="header">        
+      <img id="headerImg" src={headerImg} alt="headerImg"/>
+    </div>
+  );
 }
 
 export default Header;
