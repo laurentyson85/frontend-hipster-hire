3d3b643318c113395421338b98e271b07264ebc9
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-25 17:30:02 -0600

    Update open and expired logic

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -2,48 +2,48 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom"
 
 function JobDetails({allJobs}) {
-    const [button, setButton] = useState(true)    
-    const { id } = useParams()
-    const {title, field, position, key_skill, open, expired, employment, company: {
-            logo_url,
-            hired_hipsters,
-            name,
-            slogan
-        },
-        } = allJobs.find(job => job.id === parseInt(id))
-
-    function handleHireClick(){
-        console.log("hired a hipster")
-    }
     
-    function handleDeleteClick(){
-        console.log("deleted that job")
-    }    
+  const { id } = useParams()
+  const {title, field, position, key_skill, open, expired, employment, company: {
+          logo_url,
+          hired_hipsters,
+          name,
+          slogan
+        }
+      } = allJobs.find(job => job.id === parseInt(id))
+
+  function handleHireClick(){
+    console.log("hired a hipster")
+  }
+
+  function handleDeleteClick(){
+    console.log("deleted that job")
+  }
+  
 
     
-    return (
-        <div className="jobDetails">                   
-            <img src={logo_url} alt="company logo" />
-            <h3>{name}</h3>
-            <p>{hired_hipsters}</p> 
-            <p><span style={{fontWeight: "bold"}}>Company slogan</span> {slogan}</p>
-            <p>We're hiring a {title}. This {field} {position} is skilled in: {key_skill}</p>
-            {expired? (
-                <p>The position is expired. We weren't able to hire a hipster in time. Delete it</p>
-                    ) : (
-                <p>Active job posting</p>
-            )}
-            
-            <p><span style={{fontWeight: "bold"}}>Employment Type</span> {employment}</p>
-            {open? (
-                <button className="open job" onClick={handleHireClick}>Hire a Hipster</button>
-                    ) : (
-                null
-            )}
+  return (
+      <div className="jobDetails">                   
+          <img src={logo_url} alt="company logo" />
+          <h3>{name}</h3>
+          <p>{hired_hipsters}</p> 
+          <p><span style={{fontWeight: "bold"}}>Company slogan</span> {slogan}</p>
+          <p>We're hiring a {title}. This {field} {position} is skilled in: {key_skill}</p>
+          {expired? (
+              <p>The position is expired. We weren't able to hire a hipster in time. Delete it</p>
+                  ) : (
+              <p>Active job posting</p>
+          )}        
+          <p><span style={{fontWeight: "bold"}}>Employment Type</span> {employment}</p>
+          {open && !expired?(
+            <button className="open job" onClick={handleHireClick}>Hire a Hipster</button>
+                  ) : (
             <button className="expired" onClick={handleDeleteClick}>Need to delete? Click here</button>
-        </div>
+          )}
+         
+       </div>
       )
-}
+   } 
 
 export default JobDetails;
 
