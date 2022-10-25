01c311909853d814a376058d1028eb0934ea5389
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-25 16:37:46 -0600

    Remove if logic from JobDetails

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,28 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom"
 
 function JobDetails({allJobs}) {
-    
-const { id } = useParams()
-
-function handleHireClick(){
-    console.log("hired a hipster")
-}
-
-function handleDeleteClick(){
-    console.log("deleted that job")
-}
-
-if (allJobs.length > 0){
+    const [button, setButton] = useState(true)    
+    const { id } = useParams()
     const {title, field, position, key_skill, open, expired, employment, company: {
-        logo_url,
-        hired_hipsters,
-        name,
-        slogan
-    },
-    } = allJobs.find(job => job.id === parseInt(id))
+            logo_url,
+            hired_hipsters,
+            name,
+            slogan
+        },
+        } = allJobs.find(job => job.id === parseInt(id))
 
-    //clean up open and expired logic and text. display some combo of filled, open and avtive, and open and expired.
+    function handleHireClick(){
+        console.log("hired a hipster")
+    }
+    
+    function handleDeleteClick(){
+        console.log("deleted that job")
+    }    
 
     
     return (
@@ -46,10 +42,7 @@ if (allJobs.length > 0){
             )}
             <button className="expired" onClick={handleDeleteClick}>Need to delete? Click here</button>
         </div>
-      ); 
-  } else{
-     return null
-  }
+      )
 }
 
 export default JobDetails;
