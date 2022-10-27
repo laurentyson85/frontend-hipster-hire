84a8313e90492c070bdf0d8c58ae3a1f70618bc8
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-27 07:59:18 -0600

    Fetch hipsters

--- src/components/HipsterForm.js
+++ src/components/HipsterForm.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function HipsterForm() {
+  
+
+
   return (
     <div>form</div>
     
--- src/components/HipsterView.js
+++ src/components/HipsterView.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function HipsterView() {
+  const [allHipsters, setAllHipsters] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/hipsters")
+    .then(response => response.json())
+    .then(data => setAllHipsters(data))
+  }, [])
+
+
+
+
   return (
     <div>hipsters</div>
     
