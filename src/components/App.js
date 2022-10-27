0cf542dbddae4855cec63583cabf3b3f40342eeb
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-26 21:06:30 -0600

    Add code to handle delete

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import JobDetails from './JobDetails';
 
 function App() {
   const [allJobs, setAllJobs] = useState([])
-  const [hipsterCount, setHipsterCount] = useState(25) //update hipsterCount state when new hipster is created
+  const [hipsterCount, setHipsterCount] = useState(25) //to do: update hipsterCount state when new hipster is created
 
   useEffect(() => {
     fetch("http://localhost:9292")
@@ -20,8 +20,9 @@ function App() {
   }, [])
 
  const hiredHipster = Math.floor(Math.random() * `${hipsterCount}`) + 1
-  
-//have something visually happen to show a hipster was hired
+
+ //maybe move this to the details level and send state up
+//this code works, but have something visually happen to show a hipster was hired
   function onHireHipster(id){
     fetch(`http://localhost:9292/jobs/${id}`, {
       method: "PATCH",
@@ -46,18 +47,18 @@ function App() {
     });  
   }
 
+ //maybe move this to the details level and send state up 
+//this code works, add useHistory hook here 👇 to route user somewhere else when job is deleted. Maybe route to new job posting
   function onDeleteJob(id){
     fetch(`http://localhost:9292/jobs/${id}`,{
       method: "DELETE",      
   })
   .then(response => response.json())
-  .then((data) => {
-      const updatedAllJobs = allJobs.filter((job) => job.id !== id)
-      console.log(updatedAllJobs)
-      console.log(data)
+  .then(() => {
+      const updatedAllJobs = allJobs.filter((job) => job.id != id)
+      setAllJobs(updatedAllJobs)
     })
   }
-
 
 
   return (    
