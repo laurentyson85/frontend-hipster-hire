93ec3cf29066308cd4e81ae508b0377509501e2a
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-27 20:14:30 -0600

    Add filter logic

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Filter() {
-  return (
-    <div>hold</div>
-    
-  )
+function Filter(handleFilter) {
+  return (    
+    <div className="filter">
+      <label>Filter by Availability</label>        
+      <select name="filter" onChange={handleFilter} >
+        <option value="All">All</option>
+        <option value="true">Active Jobs</option>
+        <option value="false">Past Openings</option>
+      </select>
+    </div>
+);
 }
 
 export default Filter;
