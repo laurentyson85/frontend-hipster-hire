7e455a40abe5cf77cb057d65fb94c5b779578f57
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-29 10:54:27 -0600

    Make new component annd create next and back buttons

---
+++ src/components/BackButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function backButton({handleBack}) {
+  return (
+    <button onClick={handleBack}>◀️</button>    
+  )
+}
+export default backButton;
---
+++ src/components/NextButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function NextButton({handleNext}) {
+  return (
+    <button onClick={handleNext}>▶️</button>    
+  )
+}
+export default NextButton;
