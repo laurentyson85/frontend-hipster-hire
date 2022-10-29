cacd65a9711e5dbbe793f48fb6ea6224a630a2d2
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-28 19:02:42 -0600

    Add new component for hipster details

---
+++ src/components/HipsterDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router-dom"
+
+function HipsterDetails() {
+    return (
+      <div>Hipster details</div>
+      
+    )
+  }
+  
+  export default HipsterDetails;
