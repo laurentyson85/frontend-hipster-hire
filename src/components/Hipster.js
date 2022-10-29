7a198c47a92c830214dae74235f2026592484c03
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-28 21:25:52 -0600

    Add job details for each hipster

--- src/components/Hipster.js
+++ src/components/Hipster.js
@@ -1,24 +1,29 @@
 import React from "react";
 import mustache from "../images/mustache.png"
 
-function Hipster({name, bio, jobs}) { 
+function Hipster({name, bio, jobs}) {
+  
+  const hipsterJobs = jobs.map(job =>
+    <div key={job.id}>
+      <p>{job.title}</p>
+      <p>{job.position}</p>
+      <p>{job.employment}</p>
+      <p>{job.company_name}</p>
+    </div>
+    
+    )
 
   return (
     <li className="card">
       <p>{name}</p>
       <img slassName ="mustacheImage" src={mustache} alt="hipster mustache"/>
-      <p>{bio}</p>      
+      <p>{bio}</p>
+      <div className="hipsterjobs">
+        {hipsterJobs}
+      </div>
       </li>    
   )
 }
 
 export default Hipster;
 
-{/* <div>
-        <p><span style={{fontWeight: "bold"}}>Name:</span> {name}</p>
-        <p>{bio}</p>
-        <p><span style={{fontWeight: "bold"}}>Employment Type</span></p>
-        <p>{hipsterJobs}</p>
-      </div>    */}
-
-      
