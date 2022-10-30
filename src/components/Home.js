526a744a525b26410dd2cce38a058fd60a291b7d
Lauren Tyson <lauren.wlkr@gmail.com>
2022-10-30 13:52:16 -0600

    Change text

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className="home">
           <h2>Welcome to Hipster Hire job board!</h2>
-          <p>If you're a hipster looking for employment, stroll over to Hipster View 🤘</p>
+          <p>If you're a hipster looking for employment, stroll over to Hipster View to join the crew 🤘</p>
           <p>If you're here to add a job, head to Job Posting 📝 </p>
           <p>To see all jobs and hire a hipster, check out All Jobs! 🙌</p>
         </div>
